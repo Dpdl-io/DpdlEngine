ab880bd0cfbd478da08be819391fbb778dc431c9
Armin Costa <armincost@gmail.com>
2023-11-02 23:47:54 +0100

    added msg paramter to raise(..)

--- DpdlLibs/testRaiseException.h
+++ DpdlLibs/testRaiseException.h
@@ -11,22 +11,22 @@ println("testing str raise(..)")
 println("")
 
 string s1 = "nul(l)"
-raise(s1)
+raise(s1, "s1 is null")
 
 println("testing int raise")
 int i = 1
-raise(i)
+raise(i, "i == -1")
 
 println("testing bool raise")
 bool b = true
-raise(b)
+raise(b, "b == false")
 
 println("testing object raise")
-object o =  loadObj("String", "test")
-raise(o)
+object o = loadObj("String", "test")
+raise(o, "o == null")
 
 println("testing bool expression raise")
-raise(o =! null)
+raise(o =! null, "o =! null")
 
 dpdl_print_exception_table()
 
