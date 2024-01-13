079511d98e1f1c6054b53f949a876b56477b9ad1
Armin Costa <armincost@gmail.com>
2024-01-13 08:58:30 +0100

    add Dpdl example app

---
+++ DpdlLibs/app/getnews/dpdlGetNews.h
@@ -0,0 +1,50 @@
+# File: app/getnews/dpdlGetNews.h
+#
+# Example: Download top 10 story news via json http get requests. Data is decoded and stored is a native java object via getter and setter
+#
+# Author: A.Costa
+# e-mail: info@dpdl.io
+#
+#
+
+import('http')
+import('json')
+
+
+# main
+string stories_url   = "https://hacker-news.firebaseio.com/v0/topstories.json"
+string item_base_url = "https://hacker-news.firebaseio.com/v0/item/"
+
+println("downloading news...")
+
+string resp = http.get(stories_url)
+
+raise(resp, "Error in downloading data")
+
+object jsonobj = json.parse(resp, 0)
+ids[] = array(jsonobj)
+
+string story_url
+object story
+object storyobj = loadObj("Story")
+
+int c = 0
+for(c < 10)
+
+	story_url = item_base_url + ids[c] + ".json"
+	resp = http.get(story_url)
+
+	raise(resp, "Error in downloading story")
+
+	storyobj = json.decode(resp, storyobj)
+
+	println("id: " + storyobj.id)
+	println("title: " + storyobj.title)
+	println("url: " + storyobj.url)
+
+	println("----------------------------------------")
+
+	c = c+1
+endfor
+
+println("finished!")
