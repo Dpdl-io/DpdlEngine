88b135bf7ad36037c9d51a43792e2b9ba4fbd7e8
Armin Costa <armincost@gmail.com>
2024-03-07 04:17:25 +0100

    added Dpdl example with embedded julia code to download HTTP data

--- DpdlLibs/julia/dpdlJuliaHttp.h
+++ DpdlLibs/julia/dpdlJuliaHttp.h
@@ -1,4 +1,4 @@
-# File: julia/dpdlJuliaPlot.h
+# File: julia/dpdlJuliaHttp.h
 #
 # Example: This Dpdl script executes an embedded Julia script that downloads data via HTTP and saves the result in a file
 #
@@ -8,41 +8,63 @@
 #
 
 # main
-
 println("Testing HTTP get data with embedded Julia code...")
 
+string url_to_download_1 = "{    \"file_name\": \"see_solutions.html\" , \"url_download\": \"http://www.seesolutions.it\"    }"
+string url_to_download_2 = "{    \"file_name\": \"info_see.jpg\" , \"url_download\": \"http://www.seesolutions.it/images/intro_see.jpg\"    }"
+
+object url1 = loadObj("String", url_to_download_1)
+object url2 = loadObj("String", url_to_download_2)
+
+dpdl_stack_push(url1, url2)
 >>julia
 using JSON
 using HTTP
 
 function download_content_from_url(json_file::String)
-	println("parsing json...")
+	println("parsing json data...")
     data = JSON.parse(json_file)
 	println("done")
+	println("")
 
+	file_n = data["file_name"]
     url = data["url_download"]
 
-    response = HTTP.get(url)
+	println("file: $file_n")
+	println("url: $url")
+
+	println("")
+
+	response = HTTP.get(url)
 
     if response.status == 200
-        filename = "./Test/downloaded_content.txt"
+		println("data downloaded successfully, saving file...")
+
+        filename = "./Test/$file_n"
         open(filename, "w") do file
             write(file, response.body)
         end
-        println("Content downloaded successfully to $filename")
+
+        println("Content saved in file $filename")
     else
-        println("Failed to download content. HTTP status code: $(response.status)")
+		println("Failed to download content. HTTP status code: $(response.status)")
     end
 end
 
-json = "{    \"url_download\": \"http://www.seesolutions.it\"   }"
-println("download data in json: ")
-println(json)
-download_content_from_url(json)
+function dpdl_main(param)
+	for p in param
+		println("param: $p")
+		json = p
+		println("download data contained in json: ")
+		println(json)
+		download_content_from_url(json)
+	end
 
+	dispose_status = @ccall dpdl_julia_dispose()::Int32
 
-dispose_status = @ccall dpdl_julia_dispose()::Int32
-return 1
+	return 1
+end
+
 <<
 
 int exit_code = dpdl_exit_code()
